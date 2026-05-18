122bd8335a7c1cbbfd4f4b91473f7c06b9eb63cc
Denilson Junior <denilson.junior@dafiti.com.br>
2026-05-18 15:16:40 -0300

    update

--- script-blog.js
+++ script-blog.js
@@ -1,1 +1,18 @@
-console.log("oi");
+setTimeout(() => {
+    var animation = { duration: 5000, easing: (t) => t }
+
+    var slider = new KeenSlider("#my-keen-slider", {
+        loop: true,
+        renderMode: "performance",
+        drag: false,
+        created(s) {
+            s.moveToIdx(5, true, animation)
+        },
+        updated(s) {
+            s.moveToIdx(s.track.details.abs + 5, true, animation)
+        },
+        animationEnded(s) {
+            s.moveToIdx(s.track.details.abs + 5, true, animation)
+        },
+    })
+}, 5000);
